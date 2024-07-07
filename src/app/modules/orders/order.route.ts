85a9e7acf007533a061dd51d3c109d8fd333ef37
ismailJihad29 <ismailhossenjihad234@gmail.com>
2024-07-07 19:23:54 +0600

    get order by email sucess

--- src/app/modules/orders/order.route.ts
+++ src/app/modules/orders/order.route.ts
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/', OrderController.createOrder);
 router.get("/", OrderController.getAllOrder);
-// router.get("/", OrderController.getOrderByEmail);
+router.get("/", OrderController.getOrderByEmail);
 
 export const OrderRoutes = router;
